59da4ba9d76500810cba574c9311d188215cdd32
Kysfaggit <36162766+Kysfaggit@users.noreply.github.com>
2018-06-12 16:05:16 +0200

    Task6

---
+++ PaleonT/2018_2/src/Task6.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+	kss()
+}
+
+fun kss (){
+	var jiddishHQ :Array<Int> = arrayOf(26,35,25,68,24,65,532,2,25,852,82,285,285,82,83,83,42,345,3,453,42,3,42)
+	for (x in jiddishHQ){
+		if ( x % 2 == 1 ) {
+			println(x)
+		}
+	}
+
+
+
+
+}
