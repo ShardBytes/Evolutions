820a732d9c6639b0b1869fa5a09dd2a80fb80ba0
Kysfaggit <36162766+Kysfaggit@users.noreply.github.com>
2018-06-12 16:19:00 +0200

    Task7

---
+++ PaleonT/2018_2/src/Task7.kt
@@ -0,0 +1,15 @@
+
+/*for (x in jiddishHQ){
+
+
+	}*/
+
+var jiddishHQ :Array<Int> = arrayOf(26,35,25,68,24,65,532,2,25,852,82,285,285,82,83,83,42,345,3,453,42,3,42)
+
+fun parne (){
+	for(x in jiddishHQ){
+
+	var parne:ArrayList<Int> = ArrayList()
+
+
+}
