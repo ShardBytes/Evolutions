bfec59f1759f7de865a24663c4d3ad94c3d755d8
RandomTypek <31035586+RandomTypek@users.noreply.github.com>
2018-06-15 15:28:45 +0200

    hmm

--- RandomTypek/2018_2/src/K_7.kt
+++ RandomTypek/2018_2/src/K_7.kt
@@ -1,15 +1,16 @@
-fun check(c : Array<Int>) {
-	for(i in c){
-		if(i%2 = 0){
-
-
-		}
-	}
-}
 
 fun main(args: Array<String>) {
-	var tabulka : Array<Int> = arrayOf(47, 5, 14, 3, 56, 420, 69, 123)
-	var tabulkaParne : Array<Int>
-	var tabulkaNeparne : Array<Int>
-	var predParne : Array<Int> = parnee(tabulka)
+	var tabulka = listOf(47, 5, 14, 3, 56, 420, 69, 123, 465668, 777)
+	var tabulkaParne = listOf<Int>()
+	var tabulkaNeparne = listOf<Int>()
+
+	for(i in tabulka){
+		if(i%2 == 0){
+			tabulkaParne += i
+		} else {
+			tabulkaNeparne += i
+		}
+	}
+
+	println(tabulkaParne[tabulkaParne.size-2] * tabulkaNeparne[tabulkaNeparne.size-2])
 }
