331a7f5d1207d1b8df4fe66697f575b9ca9ccb58
Kysfaggit <36162766+Kysfaggit@users.noreply.github.com>
2018-06-18 23:23:22 +0200

    Task10
    Gallon Of Semen

---
+++ PaleonT/TasksDokonceneDoma/src/Task10.kt
@@ -0,0 +1,10 @@
+import java.io.File
+
+fun gallonsOfSemen(){
+    var subor : File = File("C:\\Users\\PaleonT\\Desktop\\Homework quote unquote")
+    java.awt.Desktop.getDesktop().open(subor)
+}
+
+fun main(args: Array<String>) {
+    gallonsOfSemen()
+}
