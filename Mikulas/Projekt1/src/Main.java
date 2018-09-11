9d9420f18afd2400949a1110e012c0f9e29ee70b
TheMajkySK <malcovsky.mikulas@gmail.com>
2018-09-11 14:55:35 +0200

    Ahoj

---
+++ Mikulas/Projekt1/src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		int a;
+		a = 4;
+
+		int b;
+		b = 2;
+
+		int sucet;
+		sucet = a + b;
+		System.out.println(""+ a + "+" );
+	}
+}
---
+++ Mikulas/Projekt1/src/Sucet.java
@@ -0,0 +1,9 @@
+public class Sucet {
+
+	public static void main(String[] args) {
+
+		int cislo;
+		cislo = 1;
+		System.out.println(cislo);
+	}
+}
