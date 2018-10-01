88cd3bd013879484f97c7b25706624b273370e10
miruat <36480281+miruat@users.noreply.github.com>
2018-10-01 13:05:47 +0200

    nic

---
+++ MinkoTryhard/Helloworld/src/PreLivku.java
@@ -0,0 +1,30 @@
+public class PreLivku {
+    
+    public static void main(String[] args) {
+        System.out.println("Ahoj ako sa mas ?");
+        System.out.println("Mam sa celkom fajn XD ");
+        
+        String minko = "sestnast";
+        int minkoVekVCislach = 16;
+        double minkoVyska = 180.11;
+    
+        System.out.println(minko + " " + minkoVekVCislach + " " + minkoVyska);
+        /*
+        THAT
+        IS
+        GREAT
+         */
+        
+        /*
+         * jkl
+         * hjk
+         * jk
+         *
+         *
+         *
+         */
+        
+    }
+// LOVE U
+
+}
