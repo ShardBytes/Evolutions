c367d6eaaad6634e9c75bb89dc9267452e9cf7cf
RandomTypek <31035586+RandomTypek@users.noreply.github.com>
2018-10-03 15:24:55 +0200

    plz dont

---
+++ RandomTypek/2018_3/src/Vector2D.java
@@ -0,0 +1,14 @@
+public class Vector2D {
+	public int x;
+	public int y;
+
+	public Vector2D(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public Vector2D(){
+		this.x = 0;
+		this.y = 0;
+	}
+}
