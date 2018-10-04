4f7524718099f1738a17138f21412b78d93bcb42
miruat <36480281+miruat@users.noreply.github.com>
2018-10-04 19:43:37 +0200

    nic

--- MinkoTryhard/Helloworld/src/MinkoTest.java
+++ MinkoTryhard/Helloworld/src/MinkoTest.java
@@ -1,8 +1,61 @@
 public class MinkoTest {
-
-
+    
+    
     public static void main(String[] args) {
-        System.out.println("JOOOOOOOO");
-        System.out.println("JOOOOOOOO");
+        System.out.println("Ahoj ako sa mas ?");
+        System.out.println("Mam sa celkom fajn XD ");
+        
+        String minko = "sestnast";
+        int minkoVekVCislach = 16;
+        double minkoVyska = 180.11;
+        char krvnaSkupina= 'A';
+        boolean zivy = true;
+        
+        System.out.println(minko + " " + minkoVekVCislach + " " + minkoVyska + " " + krvnaSkupina + " " +zivy);
+        /*
+        THAT
+        IS
+        GREAT
+         */
+        
+        int x = 5;
+        int y = 14;
+        int z = 25;
+        
+        int sum1 = x + z;
+	    System.out.println(sum1);
+        int sum2 = x - y + z * x / y + x + sum1 ;
+	    System.out.println(sum2);
+     
+	    int sum3 = z / y;
+	    System.out.println(sum3);
+	    int sum4 = z / x ;
+	    System.out.println(sum4);
+	    
+	    double sum5 = z / y;
+	    System.out.println(sum5);
+	
+	    double sum6 = z % y; //zvysok
+	    System.out.println(sum6);
+	
+	    
+	    
+	    System.out.println(z);
+	    z++;
+	    System.out.println(z);
+	    z--;
+	    System.out.println(z);
+	
+	
+	    System.out.println(z);
+	    ++z;
+	    System.out.println(z);
+	    --z;
+	    System.out.println(z);
+	    // 35. strana
+	    
+	    
     }
+// LOVE U
+
 }
