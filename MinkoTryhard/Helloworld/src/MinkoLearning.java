e2dfff9ada3fd7dc684edd79894946c71b719fa2
miruat <36480281+miruat@users.noreply.github.com>
2018-10-04 15:39:59 +0200

    pokrok

---
+++ MinkoTryhard/Helloworld/src/MinkoLearning.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MinkoLearning {
+	
+	public static void main(String[] args) {
+		String meno = "minko";
+		String priezvisko = "dina";
+		
+		System.out.println("My name is "+ meno + " " + priezvisko);
+		
+		Scanner vstup = new Scanner(System.in);
+		System.out.println("Ahoj, ako sa máš ?");
+		System.out.println("Som rád že sa máš : " + vstup.nextLine());
+		
+		
+		
+	}
+	
+}
--- MinkoTryhard/Helloworld/src/MinkoTest.java
+++ MinkoTryhard/Helloworld/src/MinkoTest.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class MinkoTest {
-
-
+    
     public static void main(String[] args) {
-        System.out.println("JOOOOOOOO");
-        System.out.println("JOOOOOOOO");
+        Scanner vstupč = new Scanner(System.in);
+	
+	    System.out.println("Zadaj 3 - 1 čísla XD");
+	    
+	    
+	    int x = vstupč.nextInt();
+        int y = vstupč.nextInt();
+        
+	    
+	    Scanner vstupm = new Scanner(System.in);
+	    System.out.println("Zadaj meno a priezvisko");
+	    String meno = vstupm.nextLine();
+	    
+	    
+	    System.out.println(meno+" "+"tvoje IQ je: "+ (x/y));
+	    if(x/y > 150) {
+		    System.out.println("Congratulation u r genius.");
+	    }else if(x/y > 100){
+		    System.out.println("Congrat u are not stupid.");
+	    }else{
+		    System.out.println(meno+" go for Slovak rap.");
+	    }
     }
 }
