5918fbcb2d20778673a9ac315f501f032134148d
TheMajkySK <malcovsky.mikulas@gmail.com>
2018-10-17 13:57:44 +0200

    Dvojnasobič

---
+++ TheMajkySK/2018_5/src/Dvojnasobič.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Dvojnasobič {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in, "Windows-1250");
+		System.out.println("Aké čislo chceš zdvojnásobiť");
+		int a = sc.nextLine();
+		a= a * 2;
+		System.out.println(a);
+
+
+	}
+
+
+}
