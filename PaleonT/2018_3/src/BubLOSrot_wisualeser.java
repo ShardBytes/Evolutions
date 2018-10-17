cc14c2e9be138970f7b2f64b3b396a171ffee62f
PaleonT <31035586+RandomTypek@users.noreply.github.com>
2018-10-17 14:13:40 +0200

    Focken Jew

--- PaleonT/2018_3/src/BubLOSrot_wisualeser.java
+++ PaleonT/2018_3/src/BubLOSrot_wisualeser.java
@@ -1,6 +1,9 @@
 import processing.core.PApplet;
+import java.util.Scanner;
 
 public class BubLOSrot_wisualeser extends PApplet {
+	Scanner vstup = new Scanner(System.in);
+
 	private int[] Yes = {2, 36, 58, 49, 75, 26, 5, 52, 97, 5, 31, 56, 12, 45, 4, 5, 88, 4, 55, 21, 68};
 
 	public static void main(String[] args) {
@@ -23,7 +26,7 @@ public class BubLOSrot_wisualeser extends PApplet {
 		clear();
 		background(255, 255, 255);
 
-		textFont(createFont("Comic Sans MS", 16));
+		textFont(createFont("Times New Roman", 16));
 		for (int i = 0; i < Yes.length; i++) {
 			int Devat = Yes[i];
 			fill(255- Devat, 255/Devat, 255-Devat);
@@ -38,7 +41,7 @@ public class BubLOSrot_wisualeser extends PApplet {
 	@Override
 	public void mousePressed() {
 		int n = Yes.length;
-		int temp = 0;
+		int temp = 1;
 
 		for (int j = 1; j < n; j++) {
 			if (Yes[j - 1] > Yes[j]) {
