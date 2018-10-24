b8fa8169af6a9163879f35f9335a2921ca6f1b1a
Kysfaggit <36162766+Kysfaggit@users.noreply.github.com>
2018-10-24 15:26:10 +0200

    I have no focke idea
    Filip and sebu pls help  i forgot java

---
+++ PaleonT/Trojholnik/src/Trojholnik.java
@@ -0,0 +1,60 @@
+public class Trojholnik {
+	public float getA() {
+		return a;
+	}
+
+	public void setA(float a) {
+		this.a = a;
+	}
+
+	public float getB() {
+		return b;
+	}
+
+	public void setB(float b) {
+		this.b = b;
+	}
+
+	public float getC() {
+		return c;
+	}
+
+	public void setC(float c) {
+		this.c = c;
+	}
+
+	public float getAlfa() {
+		return alfa;
+	}
+
+	public void setAlfa(float alfa) {
+		this.alfa = alfa;
+	}
+
+	public float getBeta() {
+		return beta;
+	}
+
+	public void setBeta(float beta) {
+		this.beta = beta;
+	}
+
+	public float getGama() {
+		return gama;
+	}
+
+	public void setGama(float gama) {
+		this.gama = gama;
+	}
+
+	float a;
+	float b;
+	float c;
+
+	float alfa;
+	float beta;
+	float gama;
+
+	}
+
+
---
+++ PaleonT/Trojholnik/src/Vypocet.java
@@ -0,0 +1,16 @@
+public class Vypocet {
+	public static void main(String[] args) {
+
+
+
+	}
+
+
+	static float  vypocet(float A,float B,float C,float AA, float BB, float CC){
+
+
+		float a = (B* Math.sin(AA))/Math.sin(BB) || (C*Math.sin(AA))/Math.sin(CC) || Math.sqrt((B*B)-(C*C)-(2*B*B*Math.cos(AA)));
+
+		}
+	}
+
