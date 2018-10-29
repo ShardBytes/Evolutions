6c7590e9b849fb1ad4486c7c59c83bf2da6bb73d
miruat <36480281+miruat@users.noreply.github.com>
2018-10-29 10:54:54 +0100

    opakovanie

---
+++ MinkoTryhard/Opakovanie/src/com/github/minko/Opakovanie.java
@@ -0,0 +1,13 @@
+package com.github.minko;
+
+import java.util.Scanner;
+
+public class Opakovanie {
+	public static void main(String[] args) {
+		Scanner vstup = new Scanner(System.in);
+		String y = vstup.nextLine();
+		String x = vstup.nextLine();
+		
+		System.out.println(x + " " +y);
+	}
+}
