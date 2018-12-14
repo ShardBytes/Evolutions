d44d55cbd658ddf4e2b5154e76646986fda6b8fb
PabloSVK <mikitkamarek@gmail.com>
2018-12-15 00:32:49 +0100

    commit message

---
+++ Pablo/asker.py
@@ -0,0 +1,35 @@
+
+from random import randint
+cislo = (randint(0, 100))
+
+
+print("Vitaj v programe Asker")
+tip = input("Zadaj svoje číslo: ")
+
+
+ctip = int(tip)
+
+while(ctip!=cislo):
+
+    if ctip < cislo:
+        ctip = int(input("Zadaj väčšie číslo: "))
+    else:
+        ctip = int(input("Zadaj menšie číslo: "))
+
+
+if ctip == cislo:
+    print("Uhadol si číslo!")
+
+
+#if
+#else
+#else if = elif
+
+
+
+
+
+
+
+#else
+ #statement(s)
---
+++ Pablo/sucet.py
@@ -0,0 +1,7 @@
+
+prve = int(input("Zadaj prve cislo: "))
+druhe = int(input ("Zadaj druhe cislo: "))
+
+vysledok = prve + druhe
+
+print("Sucet je: ", vysledok ,)
