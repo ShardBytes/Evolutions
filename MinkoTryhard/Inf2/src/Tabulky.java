e670906e7bbabb8e778d1e81e06507ca24982f77
miruat <36480281+miruat@users.noreply.github.com>
2019-06-10 13:05:29 +0200

    3,14skvorky

---
+++ MinkoTryhard/Inf2/src/Tabulky.java
@@ -0,0 +1,28 @@
+import processing.core.PApplet;
+import java.util.Random;
+
+public class Tabulky extends PApplet {
+	
+	
+	Random random = new Random();
+	
+	public static void main(String[] args) {
+		PApplet.main("Tabulky");
+	}
+	
+	public void settings() {
+		size(320 ,240);
+		
+	}
+	int x = 10;
+	int y = 10;
+	
+	@Override
+	public void setup(){
+		
+		int[] mojaTabulka = {1,2,3,3};
+		
+		
+		
+	}
+}
--- MinkoTryhard/Inf2/src/Tictactoe.java
+++ MinkoTryhard/Inf2/src/Tictactoe.java
@@ -19,8 +19,13 @@ public class Tictactoe extends PApplet {
 	int smallerSize = 0;
 	int cellOffsetX = 0;
 	int cellOffsetY = 0;
-	int cellCount = 5;
+	int cellCount = 3;
 	
+	int whichPlayer = 0;
+	
+	int[][]cellData = {{0,0,0},
+					   {0,0,0},
+			           {0,0,0}};
 	
 	@Override
 	public void setup() {
@@ -31,6 +36,7 @@ public class Tictactoe extends PApplet {
 	public void draw() {
 		clear();
 		table();
+		drawData();
 	}
 	
 	public void table() {
@@ -56,6 +62,94 @@ public class Tictactoe extends PApplet {
 			}
 		}
 		
+	}
+	
+	public void drawData(){
+		for(int i = 0; i < cellCount; i++){
+			for(int j = 0; j < cellCount; j++ ){
+				int cell = cellData[j][i];
+				if(cell==1){
+					drawX(i * (smallerSize / cellCount) + cellOffsetX, j * smallerSize/cellCount + cellOffsetY,smallerSize/cellCount);
+				}else if ( cell == -1){
+					drawO(i * (smallerSize / cellCount) + cellOffsetX, j * smallerSize/cellCount + cellOffsetY,smallerSize/cellCount);
+				}
+			}
+		}
+		
+		
+	}
+	
+	@Override
+	public void mouseClicked() {
+		int []clickedCell = getCellFromPos();
+		System.out.println(clickedCell);
+		
+		if (clickedCell != null){
+			if(whichPlayer == 0){
+				cellData[clickedCell[1]][clickedCell[0]] = 1;
+				whichPlayer = 1;}
+			else {
+				cellData[clickedCell[1]][clickedCell[0]] = -1;
+				whichPlayer = 0;
+			}
+		}
+		
+		
+		
+		
+	
+	
+	}
+	
+	public void drawX (int pozX ,int pozY ,int cellSize){
+		stroke(255, 0, 0);
+		strokeWeight(5);
+		
+		float topLeftX = 0.2f * cellSize + pozX;
+		float topLeftY = 0.2f * cellSize + pozY;
+		
+		float topRightX = 0.8f * cellSize +pozX;
+		float topRightY = 0.2f * cellSize +pozY;
+		
+		float bottomLeftX = 0.2f * cellSize +pozX;
+		float bottomLeftY = 0.8f * cellSize +pozY;
+		
+		float bottomRightX = 0.8f * cellSize +pozX;
+		float bottomRightY = 0.8f * cellSize +pozY;
+		
+		line(topLeftX,topLeftY,bottomRightX,bottomRightY);
+		line(topRightX,topRightY,bottomLeftX,bottomLeftY);
+		
+	}
+	public void drawO(int pozX, int pozY, int cellSize) {
+		stroke(0, 0, 255);
+		strokeWeight(5);
+		
+		float centerX = 0.5f * cellSize + pozX;
+		float centerY = 0.5f * cellSize + pozY;
+		
+		ellipse(centerX, centerY, 0.6f * cellSize, 0.6f * cellSize);
+	}
+	
+	public int[] getCellFromPos(){
+		for ( int i = 0; i < cellCount; i++){
+			for(int j = 0; j < cellCount; j++){
+				int topX = i * (smallerSize/cellCount)+cellOffsetX;
+				int topY = j * (smallerSize/cellCount)+cellOffsetY;
+				
+				int bottomX = i * (smallerSize/cellCount)+cellOffsetX + (smallerSize/cellCount);
+				int bottomY = j * (smallerSize/cellCount)+cellOffsetY + (smallerSize/cellCount);
+			
+				if(mouseX > topX && mouseX < bottomX && mouseY > topY && mouseY < bottomY){
+					int[] returnValue = {i, j};
+					return returnValue;
+				}
+			
+			}
+			
+		}
+		return null;
+	
 	}
 	
 }
