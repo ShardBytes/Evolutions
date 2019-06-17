506dbf04a6b2a2dc3f2ba9c70d17d9a6fe601e51
miruat <36480281+miruat@users.noreply.github.com>
2019-06-17 13:09:44 +0200

    3,14skvorky

--- MinkoTryhard/Inf2/src/Tictactoe.java
+++ MinkoTryhard/Inf2/src/Tictactoe.java
@@ -1,5 +1,6 @@
 import processing.core.PApplet;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Tictactoe extends PApplet {
@@ -23,6 +24,13 @@ public class Tictactoe extends PApplet {
 	
 	int whichPlayer = 0;
 	
+	int gameState = 0;
+	int gameWinner = 0;
+	int blueWins = 0;
+	int redWins = 0;
+	
+	int timer = 0;
+	
 	int[][]cellData = {{0,0,0},
 					   {0,0,0},
 			           {0,0,0}};
@@ -35,8 +43,15 @@ public class Tictactoe extends PApplet {
 	@Override
 	public void draw() {
 		clear();
+		if (gameState == 0){
 		table();
 		drawData();
+		detectWins();
+		drawScore();
+		}else if(gameState == 1){
+			drawWinScreen();
+		}
+		
 	}
 	
 	public void table() {
@@ -44,12 +59,12 @@ public class Tictactoe extends PApplet {
 		int xOffsetSize = width / 5;
 		int yOffsetSize = height / 5;
 		
-		int smallerSize = width < height ? width : height;
+		this.smallerSize = width < height ? width : height;
 		int smallerOffset = xOffsetSize < yOffsetSize ? xOffsetSize : yOffsetSize;
 		
-		smallerSize = smallerSize - smallerOffset;
-		int cellOffsetX = (width - smallerSize) / 2;
-		int cellOffsetY = (height - smallerSize) / 2;
+		this.smallerSize = smallerSize - smallerOffset;
+		this.cellOffsetX = (width - smallerSize) / 2;
+		this.cellOffsetY = (height - smallerSize) / 2;
 		
 		stroke(255);
 		strokeWeight(1);
@@ -81,19 +96,22 @@ public class Tictactoe extends PApplet {
 	
 	@Override
 	public void mouseClicked() {
-		int []clickedCell = getCellFromPos();
-		System.out.println(clickedCell);
+		int[] clickedCell = getCellFromPos();
+		System.out.println(Arrays.toString(clickedCell));
 		
-		if (clickedCell != null){
-			if(whichPlayer == 0){
-				cellData[clickedCell[1]][clickedCell[0]] = 1;
-				whichPlayer = 1;}
-			else {
-				cellData[clickedCell[1]][clickedCell[0]] = -1;
-				whichPlayer = 0;
+		if (clickedCell != null) {
+			
+			if (cellData[clickedCell[1]][clickedCell[0]] == 1) {
+				
+				if (whichPlayer == 0) {
+					cellData[clickedCell[1]][clickedCell[0]] = 1;
+					whichPlayer = 1;
+				} else {
+					cellData[clickedCell[1]][clickedCell[0]] = -1;
+					whichPlayer = 0;
+				}
 			}
 		}
-		
 		
 		
 		
@@ -104,6 +122,7 @@ public class Tictactoe extends PApplet {
 	public void drawX (int pozX ,int pozY ,int cellSize){
 		stroke(255, 0, 0);
 		strokeWeight(5);
+		
 		
 		float topLeftX = 0.2f * cellSize + pozX;
 		float topLeftY = 0.2f * cellSize + pozY;
@@ -132,13 +151,13 @@ public class Tictactoe extends PApplet {
 	}
 	
 	public int[] getCellFromPos(){
-		for ( int i = 0; i < cellCount; i++){
+		for(int i = 0; i < cellCount; i++){
 			for(int j = 0; j < cellCount; j++){
-				int topX = i * (smallerSize/cellCount)+cellOffsetX;
-				int topY = j * (smallerSize/cellCount)+cellOffsetY;
+				int topX = i * (smallerSize/cellCount) + cellOffsetX;
+				int topY = j * (smallerSize/cellCount) + cellOffsetY;
 				
-				int bottomX = i * (smallerSize/cellCount)+cellOffsetX + (smallerSize/cellCount);
-				int bottomY = j * (smallerSize/cellCount)+cellOffsetY + (smallerSize/cellCount);
+				int bottomX = i * (smallerSize/cellCount) + cellOffsetX + (smallerSize/cellCount);
+				int bottomY = j * (smallerSize/cellCount) + cellOffsetY + (smallerSize/cellCount);
 			
 				if(mouseX > topX && mouseX < bottomX && mouseY > topY && mouseY < bottomY){
 					int[] returnValue = {i, j};
@@ -148,8 +167,149 @@ public class Tictactoe extends PApplet {
 			}
 			
 		}
+		
+		//int[] retV = {(width - cellOffsetX) / (smallerSize / cellCount) , (height - cellOffsetY) / (smallerSize / cellCount)};
+		//print(Arrays.toString(retV));
 		return null;
 	
+	}
+	
+	public void detectWins(){
+		
+		for(int i = 0; i < cellCount; i++){
+			int[] row = cellData[i];
+			int sum = arraySum(row);
+			
+			if( sum == cellCount) {
+				playerWon(1);
+				return;
+			}else if( sum == -cellCount){
+				playerWon(2);
+				return;
+			}
+			
+			
+		}
+		
+		for( int i = 0; i < cellCount; i++){
+			int[] column = new int[cellCount];
+			for(int j = 0; j < cellCount; j++) {
+				column[j] = cellData[j][i];
+			}
+			int sum = arraySum(column);
+			
+			if( sum == cellCount) {
+				playerWon(1);
+				return;
+			}else if( sum == -cellCount){
+				playerWon(2);
+				return;
+			}
+			
+		}
+		
+		int[] diagArr1 = new int[cellCount];
+		int[] diagArr2 = new int[cellCount];
+		
+		for(int diagonal = 0; diagonal < cellCount; diagonal++){
+			diagArr1[diagonal] = cellData[diagonal][diagonal];
+			diagArr2[diagonal] = cellData[cellCount - (diagonal + 1)][diagonal];
+			
+		}
+		int sum1 = arraySum(diagArr1);
+		int sum2 = arraySum(diagArr2);
+		
+		if(sum1 == cellCount){
+			playerWon(1);
+			return;
+			
+		}else if ( sum1 == -cellCount){
+			playerWon(2);
+			return;
+		}
+		
+		if(sum2 == cellCount){
+			playerWon(1);
+			return;
+			
+		}else if ( sum2 == -cellCount){
+			playerWon(2);
+			return;
+		}
+		
+		if(arrayFull(cellData)){
+			playerWon(0);
+			return;
+		}
+		
+	}
+	
+	public boolean arrayFull(int[][] arr){
+		for(int[] row : arr){
+			for(int cell : row) {
+				if(cell == 0){
+					return false;
+				}
+				
+			}
+		}
+		return true;
+		
+	}
+	
+	public void playerWon(int playerNumber){
+		
+		if(playerNumber == 1){
+			this.redWins = redWins + 1;
+		}else if (playerNumber ==2){
+			this.blueWins = blueWins +1;
+		}
+		
+		this.gameWinner = playerNumber;
+		this.gameState = 1;
+	}
+	
+	public void drawWinScreen(){
+		textAlign(CENTER,CENTER);
+		textSize(20);
+		if(gameWinner == 1){
+			fill(255,0,0);
+			text("Player X won",width/2,height,2);
+		}else if(gameWinner == 2){
+			fill(0,0,255);
+			text("Player 0 won",width/2,height,2);
+		}else{
+			fill(255,255,0);
+			text("Draw",width/2,height,2);
+		}
+		this.timer = timer + 1;
+		if (timer == 240){
+			gameState = 0;
+			timer = 0;
+			cellData = {{0,0,0,},
+						{0,0,0,},
+						{0,0,0,}};
+		}
+		
+	}
+	
+	public void drawScore(){
+		textAlign(CENTER,CENTER);
+		textSize(20);
+		fill(255,0,0);
+		text(redWins,width/10,height/2);
+		fill(0,0,255);
+		text(blueWins,width*9/10,height/2);
+	}
+	
+	public int arraySum(int[] arr) {
+		int sum = 0;
+		for(int element : arr) {
+			sum += element;
+		}
+		
+		return sum;
+		
 	}
 	
 }
