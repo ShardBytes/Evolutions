581b6070bdd3afaaa193110e50628b563065ced6
Frantisekkk <46000279+Frantisekkk@users.noreply.github.com>
2019-09-11 09:23:26 +0200

    suicide

---
+++ PaleonT/PythonFGTS/Erste.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+
+a = input("niggerundAyo")
+b = input("niggero b")
+c = sqrt(int(a)**2+int(b)**2)
+print(ro42und(c, 2))
---
+++ PaleonT/PythonFGTS/meno.py
@@ -0,0 +1,9 @@
+
+
+meno = input("gib name faggit: ")
+
+i = 0
+while i < 10:
+    print(meno, end=", ")
+    i=i+1
+
