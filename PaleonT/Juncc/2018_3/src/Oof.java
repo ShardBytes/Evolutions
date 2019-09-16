bf84bf6be7ce06949153f699fb2634ae5bf8b297
Kysfaggit <36162766+Kysfaggit@users.noreply.github.com>
2019-09-16 21:12:09 +0200

    Create Oof.java

---
+++ PaleonT/Juncc/2018_3/src/Oof.java
@@ -0,0 +1,13 @@
+public class Oof {
+	public static void main(String[] args) {
+
+		int A[]= {1, 2, 3, 4};
+		int x = 0;
+
+		for (int i =0; i<=A.length;i++){
+			x=x+A[i];
+		}
+		System.out.println(x);
+
+	}
+}
