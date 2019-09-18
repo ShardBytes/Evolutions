03a08d853b176fa560f85bf28a8a68a97904e0a7
Frantisekkk <46000279+Frantisekkk@users.noreply.github.com>
2019-09-18 09:22:00 +0200

    Update Also-Sprach_zarathustra.py

--- PaleonT/PythonFGTS/ahh yes/Also-Sprach_zarathustra.py
+++ PaleonT/PythonFGTS/ahh yes/Also-Sprach_zarathustra.py
@@ -1,5 +1,6 @@
 pocet_riadkov = int(input("Pocet riadkov"))
 pocet_hviezdiciek = int(input("Pocet hviezdiciek"))
 
-for i in range(0,pocet_riadkov):
+for i in range(1,pocet_riadkov+1):
+    print(i, end="  ")
     print("*"*pocet_hviezdiciek)
