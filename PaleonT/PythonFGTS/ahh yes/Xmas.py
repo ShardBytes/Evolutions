a5e5bba182db89ebdaf68345ec4e66054110118c
Frantisekkk <46000279+Frantisekkk@users.noreply.github.com>
2019-09-26 13:34:09 +0200

    kms

--- PaleonT/PythonFGTS/ahh yes/Xmas.py
+++ PaleonT/PythonFGTS/ahh yes/Xmas.py
@@ -1,6 +1,36 @@
 n = int(input("n = "))
+ahh = 2
 
+
+for x in range(0, n+1):
+    if x == 0:
+        print("  ",end="")
+    elif x<10:
+        print(f"||{x} ",end="")
+    else:
+        print(f"||{x}",end="")
+print()
+print("____"*(n+1),end="")
+print()
+print("____"*(n+1),end="")
+print()
 for i in range(1,n+1):
+    if i<10:
+        print(i,end=" |")
+    else:
+        print(i,end="|")
+        
     for x in range(1, n+1):
-        print(i*x, end=" ")
+        
+        if i*x <10:
+            print(f"|  {i*x}",end="")
+            
+        elif i*x <100:
+            print(f"| {i*x}", end="")
+            
+        else:
+            print(f"|{i*x}" , end="")
     print()
+    print("____"*(n+1),end="")
+    print()
+    
