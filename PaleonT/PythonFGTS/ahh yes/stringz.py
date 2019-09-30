81ead67d99a0daeb56670b1499ecefa6ae363a70
stivi <46000279+Frantisekkk@users.noreply.github.com>
2019-09-30 09:47:05 +0200

    Update stringz.py

--- PaleonT/PythonFGTS/ahh yes/stringz.py
+++ PaleonT/PythonFGTS/ahh yes/stringz.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 print("zadaj koeficienty")
 a = float(input("zadaj a = "))
 b = float(input("zadaj a = "))
@@ -5,3 +7,11 @@ c = float(input("zadaj a = "))
 
 D = (b**2) - (4*a*c)
 
+x1 = 0
+x2 = 0
+if D <0:
+    print("Nemá riesenie")
+else:
+    x1 = (-b + sqrt(D))/2*a
+    x2 = (-b - sqrt(D))/2*a
+    print(f"Korene rovnice sú {x1} a {x2}")
