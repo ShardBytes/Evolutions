9c6f88be218a641801f3c1d5045f236a790df340
stivi <46000279+Frantisekkk@users.noreply.github.com>
2019-10-17 15:35:42 +0200

    efdfe

---
+++ PaleonT/PythonFGTS/ahh yes/uhufr.py
@@ -0,0 +1,18 @@
+#najvatsie fibonacciho cislo kore !< 1000 000
+
+x = 0
+y = 1
+xNext = 0
+
+
+while x+y < 1000000:
+    
+        
+    xNext = x+y
+    x , y = y, xNext
+    
+    
+        
+
+
+print(xNext)
--- PaleonT/PythonFGTS/ahh yes/uhuhu.py
+++ PaleonT/PythonFGTS/ahh yes/uhuhu.py
@@ -3,14 +3,18 @@ import time
 x = int(input("Čislo: "))
 then = time.time()
 test = 0
+poci = 0 
 
 while test**2 < x:
     test = test + 1
+    poci += 1
 
 while test**2 >x:
-    test = test - 0.000001
+    test = test - 0.001
+    poci += 1
 
-print(test)
+print(f"odmocnina je {test}")
+print(f"pocet porvnani je {poci}")
 
 noe = time.time()
 
