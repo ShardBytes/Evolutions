0dea3944bdbb755a2c760a2a26438b545e17f9b1
stivi <46000279+Frantisekkk@users.noreply.github.com>
2019-10-23 09:18:11 +0200

    Update ohoo.py

--- PaleonT/PythonFGTS/ahh yes/Tkinter/ohoo.py
+++ PaleonT/PythonFGTS/ahh yes/Tkinter/ohoo.py
@@ -1,4 +1,4 @@
-hn = int(input("Číslo: "))
+"""hn = int(input("Číslo: "))
 
 delitel = 2
 
@@ -13,3 +13,14 @@ while hn > 1:
     else:
         delitel = delitel + 1
 
+"""
+
+#Pyta vek použivatelov kím pouzivatel nezada 0 a vypíše priemerný vek
+
+pocet = 0
+vek = int(input("nigga"))
+sucet = vek
+while vek > 0:
+    sucet = sucet + vek
+    pocet += 1
+    vek = int(input("nigga"))
