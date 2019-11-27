4b90f3d5a48497c7fc767c50e6a3777b8cc5343c
stivi <46000279+Frantisekkk@users.noreply.github.com>
2019-11-27 07:53:00 +0100

    Update poof.py

--- PaleonT/PythonFGTS/struct/poof.py
+++ PaleonT/PythonFGTS/struct/poof.py
@@ -5,16 +5,7 @@ x = vstup.split(".")
 
 x[2] = x[2][2:]
 
-def nasl(cislo):
-    oof = 0
-    koof = 0
-    nas = 0
-    for i in range(0,len(cislo),2):
-        oof = int(i)+ oof
-    for i in range(1,len(cislo),2):
-        koof = int(i)+ koof
-    nas = (koof-oof)%11
-    return nas
+
 
 
 
@@ -22,15 +13,13 @@ rodne = ""
 if pohlavie == "m":
     for i in reversed(x):
         rodne =rodne +i
-    nase = nasl(rodne)
-    rodne = rodne+"/"+str(nase)
+    
     print(rodne)
 else:
     x[1]= str(int(x[1])+50)
     for i in reversed(x):
         rodne =rodne +i
-    rodne =rodne +i
-    nase = nasl(rodne)
-    rodne = rodne+"/"+str(nase)
+    
+    
     print(rodne)
 
