5bfd4347dbed94d6075a3d621d184539e9049735
stivi <46000279+Frantisekkk@users.noreply.github.com>
2020-01-15 08:42:02 +0100

    5

--- PaleonT/PythonFGTS/postupnost.py
+++ PaleonT/PythonFGTS/postupnost.py
@@ -1,1 +1,22 @@
-from random import random
+from random import *
+
+x = []
+
+for i in range(0,3):
+    x.append(randint(0,66))
+
+
+def rast(tabfel):
+    x = 0
+    x1 =x+1
+    rig = True
+    while x<len(tabfel):
+        if x <= x1:
+            continue
+        else:
+            rig = False
+
+    return rig
+
+print (x)
+print (rast(x))
