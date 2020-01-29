29d8bb42c50bfdbcd3b3a26d72107cd2f9d338cb
stivi <46000279+Frantisekkk@users.noreply.github.com>
2020-01-29 09:21:02 +0100

    Update SUdokoof.py

--- PaleonT/PythonFGTS/SUdokoof.py
+++ PaleonT/PythonFGTS/SUdokoof.py
@@ -10,3 +10,26 @@ while True :
         break
 
 print(grid[0])
+
+def check(nemero,index):
+    IsAlone = True
+    counter = 0
+    for i in range(0,8):
+        if grid[i][index] == nemero:
+            couter+=1
+        if nemero in grid[i]:
+            counter+=1
+        else:
+            continue
+    if counter>0:
+        IsAlone = False
+
+    return IsAlone
+
+while True :
+    r = random.randint(1,9)
+    for i in range(8):
+        if check(r,i):
+            grid[0].append(r)
+        if len(grid[0]) == 9:
+            break
