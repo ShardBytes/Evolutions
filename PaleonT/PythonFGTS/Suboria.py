acd3c367bb6ab99b3a94368df14df5b76f87c104
stivi <46000279+Frantisekkk@users.noreply.github.com>
2020-02-11 13:46:43 +0100

    52

--- PaleonT/PythonFGTS/Suboria.py
+++ PaleonT/PythonFGTS/Suboria.py
@@ -1,17 +1,7 @@
-s1 = open("ziaci.txt", "r")
+s1 = open("Ulohy.txt", "r")
+ulohy=""
+k = s1.readline()
 
-najstarsi = 0
-najstarsimeno = 0
-while True:
-    o = s1.readline().split()
-    print(o)
-    o[0]= int(o[0])
-    if o[0]>= 18:
-        print(o[1])
-    if o[0]>najstarsi:
-        najstarsimeno = o
-    if len(o) == 1:
-        break
-print(najstarsimeno)
+print(k)
 s1.close()
 
