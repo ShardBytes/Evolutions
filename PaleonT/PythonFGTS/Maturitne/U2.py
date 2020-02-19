000c9de9988f7bbf14140156e882512b55813908
stivi <46000279+Frantisekkk@users.noreply.github.com>
2020-02-19 09:11:54 +0100

    1

---
+++ PaleonT/PythonFGTS/Maturitne/U2.py
@@ -0,0 +1,18 @@
+s1 = open("vzor.txt","r")
+
+s1 = s1.read().replace(",","").replace(".","")
+
+s1 = s1.split("\n")
+
+dex = 0
+dlyka = 0
+slovo = ""
+
+for o in s1:
+    o = o.split(" ")
+    for i in o:
+        if len(i)>dlyka:
+            dlyka = len(i)
+            slovo = i
+            
+
